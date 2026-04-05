5e79c11e38022c87a0d28b5ee7dbc36b87e00fe5
Kyle Parsotan <35744262+Kyl67899@users.noreply.github.com>
2026-04-05 19:33:37 -0400

    Added safeRoute Laning page. Think Uber but for bus routes and students and univerisities. Track your bus route like how you would in uber or other ride sharing app.

--- firebase.ts
+++ firebase.ts
@@ -11,13 +11,13 @@ import {
 } from "firebase/remote-config"
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY ||  "AIzaSyDt-ueCTioT0vmBd2tumUNV1DZqWNeiIVo",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "saferoute-55063.firebaseapp.com",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "saferoute-55063",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "saferoute-55063.firebasestorage.app",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "194542536583",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:194542536583:web:b7123c6971cc55624fb522",
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "G-24CW1FX1ZG"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
